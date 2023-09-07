2aab7fc2e3ba44c27975c6e1cac9d06d588c72fe
Mossad054 <michaelndungu054@gmail.com>
2023-09-07 14:18:12 -0700

    5print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - a program that writes proper grammer function
+ * Return: 0 (succeed)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (o);
+}
