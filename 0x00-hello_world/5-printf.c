82d69d8d30ab2059ee02864500f40a8453831070
Mossad054 <michaelndungu054@gmail.com>
2023-09-07 14:41:00 -0700

    55

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * main - a program that writes proper grammer function
+ * main - entry point
  * Return: 0 (succeed)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (o);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
