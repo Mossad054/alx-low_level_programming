fb251826a3d65668fcd437e57d656dda1af5dd56
Mossad054 <michaelndungu054@gmail.com>
2023-09-07 14:33:31 -0700

    5prime

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - a program that writes proper grammer function
  * Return: 0 (succeed)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (o);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (o);
 }
