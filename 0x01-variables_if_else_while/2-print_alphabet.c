6173426a6ffb4f7ac3afd0e33c70fd3b662e0067
Mossad054 <michaelndungu054@gmail.com>
2023-09-09 15:57:03 -0700

    hoik

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+
 /**
  * main - Prints the alphabets
- *
+ * main - this is the main function of the task
  * Return Always: 0 (Success)
  */
-int main(void) /*int main is zero */
+
+int main(void)
 {
 	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
-		putchar(i);
+	putchar(i);
 	putchar('\n');
+
 	return (0);
+
 }
