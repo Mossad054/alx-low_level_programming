4ee5ffcb478d713b57fe5227dde43b1beb7c1150
Mossad054 <michaelndungu054@gmail.com>
2023-09-10 08:23:04 -0700

    6yt

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)/* main returns 0 program succeeds*/
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f)));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
