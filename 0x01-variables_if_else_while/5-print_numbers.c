2a20ea68fb652641f26b8572270d5f815f198d35
Mossad054 <michaelndungu054@gmail.com>
2023-09-10 04:44:11 -0700

    numner

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)/* main return 0 success*/
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	printf("%d", i);
+	printf("\n");
+	return (0);
+}
