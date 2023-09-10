c0356758dfe87324552ae541094314f02e35e916
Mossad054 <michaelndungu054@gmail.com>
2023-09-10 08:03:13 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,19 +11,19 @@ int main(void)/*main returns 0 success*/
 	{
 		for  (j = 0; j < 100; j++)
 		{
-		if (i < j)
-		{
-			putchar((i / 10) + 48);
-			putchar((i % 10) + 48);
-			putchar(' ');
-			putchar((j / 10) + 48);
-			putchar((j % 10) + 48);
-			if (i != 98 || j != 98)
+			if (i < j)
 			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
 				putchar(',');
 				putchar(' ');
+				}
 			}
-		}
 		}
 	}
 }
