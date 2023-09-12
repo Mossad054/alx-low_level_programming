525b4e406126147e6cba32c4404c8594c03dd861
Mossad054 <michaelndungu054@gmail.com>
2023-09-12 11:52:26 -0700

    1alpabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Makes alphabets lowercase
+ *
+ * Return : void 
+ */
+
+void print_alphabet(void)
+{
+char ch;
+for  (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
