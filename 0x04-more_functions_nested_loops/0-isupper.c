5d92a874fee19f1962da4181b2d402570bb2ea23
Mossad054 <michaelndungu054@gmail.com>
2023-09-15 07:52:25 -0700

    0task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
- * _isupper -prog checks for uppercase character
- * @c: Variable text
- * Return: Always 1 for sucess and 0 for failure
+ * _isupper -prog checks for uppercase characters
+ * @c: Parameter
+ * Return: 0 if sucessful
  */
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
 }
