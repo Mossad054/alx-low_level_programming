c00d54413f817f153c54a95ac4daf94de9f4a153
Mossad054 <michaelndungu054@gmail.com>
2023-09-15 11:38:47 -0700

    5mian

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers -function print numbers from 0 to 14 followed new line
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (j > 9)
+			_putchar(j / 10 + '0');
+		_putchar(j % 10 + '0');
+	}
+	_putchar('\n');
+}
