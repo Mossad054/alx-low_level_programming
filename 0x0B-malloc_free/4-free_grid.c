bb3eeb19b569c2905efb9ddbbb9a17c324386776
Mossad054 <michaelndungu054@gmail.com>
2023-10-02 02:43:06 -0700

    freegrid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - frees 2d array
+ * @grid: 2d grid
+ * @height: height dimension of the grid heap
+ * Description: frees memory of the grid heap
+ * Return: none
+ *
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
+
